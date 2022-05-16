f5170a1900ac78d1b8ed5ba29cf2f1ed38bd9e4e
kevin <kevinmerlika98@gmail.com>
2022-05-16 17:39:51 +0100

    Second Commit

--- app/src/main/java/com/example/virgin_money_app/Network/Api.kt
+++ app/src/main/java/com/example/virgin_money_app/Network/Api.kt
@@ -1,6 +1,7 @@
 package com.example.virgin_money_app.Network
 
 import com.example.virgin_money_app.Model.PeopleItem
+import com.example.virgin_money_app.Model.RoomsItem
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -8,4 +9,7 @@ interface Api {
 
     @GET("people")
     suspend fun getPeople(): Response<List<PeopleItem>>
+
+    @GET("rooms")
+    suspend fun getRooms(): Response<List<RoomsItem>>
 }
